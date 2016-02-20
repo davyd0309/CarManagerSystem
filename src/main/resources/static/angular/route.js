6c1963e24b6aaac0391533892cc37957af74ae2e
Konrad <konrad@dawydiuk.pl>
2016-02-20 13:01:02 +0100

    20-02-2016

--- src/main/resources/static/angular/route.js
+++ src/main/resources/static/angular/route.js
@@ -2,10 +2,26 @@ cmsAngularModule.config(function($routeProvider){
 $routeProvider
 
     .when("/",{
-        templateUrl : "strony/logo.html"
+        templateUrl : "strony/powitanie.html"
     })
 
     .when("/dodajUzytkownika",{
         templateUrl : "strony/dodajUzytkownika.html"
+    })
+
+    .when("/informacjeDodatkowe",{
+        templateUrl : "strony/informacjeDodatkowe.html"
+    })
+
+    .when("/dane",{
+        templateUrl : "strony/dane.html"
+    })
+
+    .when("/plan",{
+        templateUrl : "strony/plan.html"
+    })
+
+    .when("/tabela",{
+        templateUrl : "strony/tabela.html"
     })
 });
