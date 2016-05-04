d937bad4a76f6740a5d61e5eca53ebd79239c616
Konrad <konrad@dawydiuk.pl>
2016-05-04 21:43:46 +0200

    04-05-2016

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -5,61 +5,22 @@
 var addCar = function () {
     alert("Dodaje auto");
 };
-var loadGrid = function () {
-    $('#content').load('main/grid');
+
+var loadGrid = function(){
+    var data = [
+        { "Name": "John Smith", "Age": 45 },
+        { "Name": "Mary Johnson", "Age": 32 },
+        { "Name": "Bob Ferguson", "Age": 27 }
+    ];
+
+    $("#grid").igGrid({
+        dataSource: data //JSON Array defined above
+    });
 };
+
 //Grid
 //var editor; // use a global for the submit and return data rendering in the examples
 
 $(document).ready(function () {
-loadGrid();
-   var data = {
-        "data": [
-        {
-            "name": "Tiger Nixon",
-            "position": "System Architect",
-            "salary": "$320,800",
-            "start_date": "2011/04/25",
-            "office": "Edinburgh",
-            "extn": "5421"
-        },
-        {
-            "name": "Caesar Vance",
-            "position": "Pre-Sales Support",
-            "salary": "$106,450",
-            "start_date": "2011/12/12",
-            "office": "New York",
-            "extn": "8330"
-        },
-        {
-            "name": "Doris Wilder",
-            "position": "Sales Assistant",
-            "salary": "$85,600",
-            "start_date": "2010/09/20",
-            "office": "Sidney",
-            "extn": "3023"
-        },
-        {
-            "name": "Donna Snider",
-            "position": "Customer Support",
-            "salary": "$112,000",
-            "start_date": "2011/01/25",
-            "office": "New York",
-            "extn": "4226"
-        }
-    ]
-    };
-
-    $('#example').DataTable( {
-        "ajax": data,
-        "columns": [
-            { "data": "name" },
-            { "data": "position" },
-            { "data": "office" },
-            { "data": "extn" },
-            { "data": "start_date" },
-            { "data": "salary" }
-        ]
-    } );
-
+    loadGrid();
 });
